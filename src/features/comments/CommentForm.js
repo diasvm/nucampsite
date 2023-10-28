5e690e472bf7a3fd7b95731673c545ba0064ce50
Victor Dias <diasvm@hotmail.com>
2023-10-28 16:09:29 -0700

    Saved Code.

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,25 +1,73 @@
-import { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label} from "reactstrap";
+import { Formik, Field } from "formik";
 
+const CommentForm = ({ campsiteId }) => {
+  const [modalOpen, setModalOpen] = useState(false);
 
-const CommentForm = ({campsiteId}) => {
+  const handleSubmit = (e, values) => {
+    e.preventDefault();
+    const comment = {
+      campsiteId: parseInt(campsiteId),
+      rating: values.rating,
+      author: values.author,
+      text: values.commentText,
+    };
+    setModalOpen(false);
+  };
 
-        const [modalOpen, setModalOpen] = useState(false);
-
-        return (
-            <> 
-            <Button outline={true} onClick={() => setModalOpen(true)}>
-                <i className='fa fa-pencil fa-lg' /> Add Comment
-            </Button>
-            <Modal isOpen={modalOpen}>
-                <ModalHeader toggle={()=> setModalOpen(false)}>
-                    Add Comment
-                </ModalHeader>
-                <ModalBody> campsite: {campsiteId} </ModalBody>
-            </Modal>
-            </>
-        )
-
-}
+  return (
+    <>
+      <Button outline={true} onClick={() => setModalOpen(true)}>
+        <i className="fa fa-pencil fa-lg" /> Add Comment
+      </Button>
+      <Modal isOpen={modalOpen}>
+        <ModalHeader toggle={() => setModalOpen(false)}>
+          Add Comment
+        </ModalHeader>
+        <ModalBody>
+          <Formik
+            initialValues={{ rating: undefined, author: "", commentText: "" }}
+            onSubmit={handleSubmit}
+          >
+            <Form>
+              <FormGroup>
+                <Label htmlFor="rating">Rating</Label>
+                <Field name="rating" as="select" className="form-control">
+                  <option>Select...</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Field>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="author">Your Name</Label>
+                <Field
+                  name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="commentText">Comment</Label>
+                <Field
+                  name="commentText"
+                  as="textarea"
+                  rows="12"
+                  className="form-control"
+                />
+              </FormGroup>
+              <Button type="submit" color="primary">
+                Submit
+              </Button>
+            </Form>
+          </Formik>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+};
 
 export default CommentForm;
